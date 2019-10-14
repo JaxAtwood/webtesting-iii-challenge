9e391037232d5c81a01f9586678a9b375f3ab552
Jackie Atwood <takalaj@verizon.net>
2019-10-14 19:06:10 -0400

    Push to GitHub, snapshots working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,24 @@
 // Test away!
+
+/*
+Controls Component
+provide buttons to toggle the closed and locked states.
+buttons' text changes to reflect the state the door will be in if clicked
+the closed toggle button is disabled if the gate is locked
+the locked toggle button is disabled if the gate is open
+*/
+
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+
+afterEach(cleanup);
+
+it("renders", () => {
+    const { asFragment } = render(<Controls text="closed" />);
+    expect(asFragment()).toMatchSnapshot();
+});
+
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+
+//Purpose of this file?????
+//Only displays the controls and the display????
+
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+
+afterEach(cleanup);
+
+it("renders", () => {
+    const { asFragment } = render(<Dashboard text="closed" />);
+    expect(asFragment()).toMatchSnapshot();
+});
+
+
